c99e0f02501e382c2d70ff438a7d8864e4acc549
SKishchenko <userg@yandex.ru>
2024-11-23 23:05:21 +0300

    ReadMe and Docker

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
 FROM eclipse-temurin:17-jdk-alpine
 EXPOSE 5500
-ADD target/MoneyTransferServiceApp-0.0.1-SNAPSHOT.jar app.jar
+ADD target/MoneyTransferApp-0.0.1-SNAPSHOT.jar app.jar
 CMD ["java", "-jar", "app.jar"]
--- src/main/java/com/example/MoneyTransferApp/service/MoneyTransferService.java
+++ src/main/java/com/example/MoneyTransferApp/service/MoneyTransferService.java
@@ -6,7 +6,6 @@ import com.example.MoneyTransferApp.request.MoneyTransferRequest;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
-import java.math.BigDecimal;
 
 @Service
 @Slf4j
